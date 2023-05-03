1d466e577042574894b91f07d732e983e4448c36
XenaShip <109364583+XenaShip@users.noreply.github.com>
2023-05-03 11:08:08 +0300

    Update filter_sorting.sql

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -1,7 +1,7 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. заказы, доставленные в страны France, Germany, Spain (таблица orders, колонка ship_country)
 SELECT order_id FROM orders
-WHERE ship_city IN ('France', 'Germany', 'Spain')
+WHERE ship_country IN ('France', 'Germany', 'Spain')
 -- 2. уникальные города и страны, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
 SELECT DISTINCT ship_country, ship_city 
 FROM orders
