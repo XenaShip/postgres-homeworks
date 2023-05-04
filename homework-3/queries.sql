3732ff6f0324398b4c928ba9816c02083d0b234a
XenaShip <109364583+XenaShip@users.noreply.github.com>
2023-05-04 13:18:19 +0300

    Update queries.sql

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,7 +2,12 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
-
+SELECT customers.company_name, 
+	CONCAT(employees.first_name, ' ', employees.last_name) AS fio 
+FROM customers
+JOIN orders on customers.customer_id = orders.customer_id
+JOIN employees on employees.employee_id = orders.employee_id
+WHERE customers.city = 'London'
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
