5d6d919817fa1d182f56d66b63d9999aea6b14a9
XenaShip <109364583+XenaShip@users.noreply.github.com>
2023-05-04 17:51:45 +0300

    Update queries.sql

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -5,9 +5,11 @@
 SELECT customers.company_name, 
 	CONCAT(employees.first_name, ' ', employees.last_name) AS fio 
 FROM customers
-JOIN orders on customers.customer_id = orders.customer_id
-JOIN employees on employees.employee_id = orders.employee_id
+JOIN orders USING(customer_id)
+JOIN employees USING(employee_id) 
+JOIN shippers ON orders.ship_via = shippers.shipper_id 
 WHERE customers.city = 'London'
+AND employees.city = 'London' AND shippers.company_name = 'United Package'
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
